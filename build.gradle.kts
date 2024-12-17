060bfad73f36b29674776211872dec2ac07d6115
Niikelion <nicraft@tlen.pl>
2024-12-17 10:18:45 +0100

    updated requirements to not restrict future ide versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.niikelion.ic10-plugin-jetbrains"
-version = "1.3"
+version = "1.4"
 
 repositories {
   mavenCentral()
@@ -32,7 +32,6 @@ tasks {
 
   patchPluginXml {
     sinceBuild.set("232")
-    untilBuild.set("242.*")
   }
 
   signPlugin {
